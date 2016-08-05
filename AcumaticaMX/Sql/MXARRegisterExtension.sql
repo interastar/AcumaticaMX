f4fe575420131a7cab707ab04df82fb09114eaaa
Marco Villaseñor <marco@villasenor.net>
2016-08-05 16:16:26 -0500

    Corrección de esquema de CFDI

--- AcumaticaMX/Sql/MXARRegisterExtension.sql
+++ AcumaticaMX/Sql/MXARRegisterExtension.sql
@@ -10,31 +10,37 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[MXARRegisterExtension](
-	-- multi-tenancy support
+	-- Campo de soporte multiempresa
 	[CompanyID]			[int] NOT NULL DEFAULT ((0)),
 
-	-- surrogate/natural key
+	-- Llaves de relación con documento contable (ARRegister en Acumatica)
 	[DocType]			char(3) NOT NULL,
 	[RefNbr]			char(15) NOT NULL,
 
-	-- extension fields
+	-- Datos del comprobante fiscal
 	[Series]			[nvarchar](25) NULL,
-	
-	[Certificate]		[nvarchar](2500) NULL,
 	[PaymentForm]		[nvarchar](50) NULL,
 	[PaymentMethod]		[nvarchar](50) NULL,
 	[OriginAccount]		[nchar](4) NULL,
 	[PaymentConditions]	[nvarchar](100) NULL,
 
- 	[StampDate]			[smalldatetime] NULL,
-	[Uuid]				[uniqueidentifier] NULL,
-	[SatCertificateNum]	[nchar](20) NULL,
-	[CfdStamp]			[nvarchar](500) NULL,
-	[SatStamp]			[nvarchar](500) NULL,
-		
-	[QrCode]			[nvarchar](100) NULL,
+	-- Datos de sello del comprobante
+	[CertificateNum]	char(20) NULL,
+	[Certificate]		[nvarchar](2500) NULL,
+ 	[SealDate]			[smalldatetime] NULL,
+	-- Sello del comprobante
+	[Seal]				[nvarchar](500) NULL,
 
-	[TfdOriginalString]	[nvarchar](1000) NULL
+	-- Datos del timbrado del comprobante
+	[Uuid]				[uniqueidentifier] NULL,
+	[SatCertificateNum]	char(20) NULL,
+ 	[StampDate]			[smalldatetime] NULL,
+	-- Sello del SAT
+	[Stamp]				[nvarchar](500) NULL,
+
+	-- Campos de addenda
+	[QrCode]			[nvarchar](150) NULL,
+	[StampString]		[nvarchar](1000) NULL
 
 	CONSTRAINT [MXARRegisterExtension_PK] PRIMARY KEY CLUSTERED 
 	(
