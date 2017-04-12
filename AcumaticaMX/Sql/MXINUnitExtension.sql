e4b4d27a428789bff37ab6eb99b2f002ac989900
omar alvarez <omar.alvarez@interastar.com>
2017-04-12 07:30:17 -0500

    Creamos extension para vincular el ctalaogo del sat de unidades de medidas con el nativo de acumatica

---
+++ AcumaticaMX/Sql/MXINUnitExtension.sql
@@ -0,0 +1,19 @@
+﻿If Exists(Select* From sys.objects Where object_id = OBJECT_ID(N'[MXINUnitExtension]') And type in (N'U'))
+	Drop Table [dbo].[MXINUnitExtension]
+Go
+Create Table [dbo].[MXINUnitExtension]
+(
+	-- multi-tenancy support
+
+    [CompanyID]				int NOT NULL DEFAULT ((0)),
+	[RecordID]				bigint NOT NULL DEFAULT ((0)),
+
+	-- Available Sat Payment Methods
+	[SatUnits]				nvarchar(2) NULL,
+
+	CONSTRAINT MXINUnitExtension_PK PRIMARY KEY
+    (
+        [CompanyID] ASC,
+		[RecordID] ASC
+    )
+)
