0bad5319d3c8a9797266000e312d1f5b4be14d45
omar alvarez <omar.alvarez@interastar.com>
2017-04-19 15:51:41 -0500

    Creamos extension de la tabla POReceiptLine para agregar los nuevos campos de información aduanera

---
+++ AcumaticaMX/DAC/MXPOReceiptLineExt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using PX.Data;
+using PX.Objects;
+using PX.Objects.PO;
+
+namespace AcumaticaMX
+{
+    [PXTable(typeof(POReceiptLine.receiptNbr), typeof(POReceiptLine.lineNbr), IsOptional = true)]
+    public class MXPOReceiptLineExt : PXCacheExtension<POReceiptLine>
+    {
+        #region Customs
+
+        public abstract class customs : IBqlField
+        {
+        }
+
+        [PXDBString(40, IsUnicode = true)]
+        [PXUIField(DisplayName = Messages.Customs)]
+        public virtual string Customs { get; set; }
+
+        #endregion Customs
+
+        #region ImportDate
+
+        public abstract class importDate : IBqlField
+        {
+        }
+
+        [PXDBDate()]
+        [PXUIField(DisplayName = Messages.ImportDate)]
+        public virtual DateTime? ImportDate { get; set; }
+
+        #endregion ImportDate
+
+        #region RequestNbr
+
+        public abstract class requestNbr : IBqlField
+        {
+        }
+
+        [PXDBString(40, IsUnicode = true)]
+        [PXUIField(DisplayName = Messages.RequestNumber)]
+        public virtual string RequestNbr { get; set; }
+
+        #endregion RequestNbr
+
+    }
+}
---
+++ AcumaticaMX/Sql/MXPOReceiptLineExt.sql
@@ -0,0 +1,31 @@
+﻿If Exists (Select * From sys.objects Where object_id = OBJECT_ID(N'[MXPOReceiptLineExt]') And type in (N'U'))
+	Drop Table [MXPOReceiptLineExt]
+Go
+
+/****** Object:  Table [dbo].[MXPOReceiptLineExt] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MXPOReceiptLineExt](
+	-- Campo de soporte multiempresa
+	[CompanyID]			[int] NOT NULL DEFAULT ((0)),
+
+	-- Llaves de relacion de orden de compra con las lineas
+	[ReceiptNbr]		[nvarchar] (15)    NOT NULL,
+	[LineNbr]			[int] NOT NULL DEFAULT ((0)),
+
+	-- Datos de Informacion Aduanera
+	[Customs]			[nvarchar](25) NULL,
+	[ImportDate]		[smalldatetime] NULL,
+	[RequestNbr]		[nvarchar](50) NULL,
+
+	CONSTRAINT [MXPOReceiptLineExt_PK] PRIMARY KEY CLUSTERED 
+	(
+		[CompanyID] ASC,
+		[ReceiptNbr] ASC,
+		[LineNbr] ASC
+	)
+)
