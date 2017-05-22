3d0523b1504b29f3d4735d34413f5b424eb39a19
omar alvarez <omar.alvarez@interastar.com>
2017-05-22 13:11:40 -0500

    Modificacion de la tabla para guardar los datos de la recepción

---
+++ AcumaticaMX/DAC/MXINItemLotSerial.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using PX.Data;
+using PX.Objects;
+using PX.Objects.IN;
+namespace AcumaticaMX
+{
+    public class MXINItemLotSerial : IBqlTable
+    {
+
+        #region RefNbr
+
+        public abstract class refNbr : IBqlField
+        {
+        }
+
+        [PXDBString(15, IsUnicode = true, IsKey = true)]
+        public virtual string RefNbr { get; set; }
+
+        #endregion RefNbr
+
+        #region InventoryID
+
+        public abstract class inventoryID : IBqlField
+        {
+        }
+
+        [PXDBInt(IsKey = true)]
+        public virtual int? InventoryID { get; set; }
+
+        #endregion InventoryID
+
+        #region LotSerialNbr
+
+        public abstract class lotSerialNbr : IBqlField
+        {
+        }
+
+        [PXDBString(15, IsUnicode = true, IsKey = true)]
+        [PXParent(typeof(Select<PX.Objects.IN.INItemLotSerial,
+            Where<PX.Objects.IN.INItemLotSerial.lotSerialNbr,
+                Equal<Current<lotSerialNbr>>>>))]
+        public virtual string LotSerialNbr { get; set; }
+
+        #endregion LotSerialNbr
+
+        #region Customs
+
+        public abstract class customs : IBqlField
+        {
+        }
+
+        [PXDBString(40, IsUnicode = true)]
+        [PXUIField(DisplayName = Messages.Customs)]
+        public virtual string Customs { get; set; }
+
+        #endregion Customs
+
+        #region ImportDate
+
+        public abstract class importDate : IBqlField
+        {
+        }
+
+        [PXDBDate()]
+        [PXUIField(DisplayName = Messages.ImportDate)]
+        public virtual DateTime? ImportDate { get; set; }
+
+        #endregion ImportDate
+
+        #region RequestNbr
+
+        public abstract class requestNbr : IBqlField
+        {
+        }
+
+        [PXDBString(40, IsUnicode = true)]
+        [PXUIField(DisplayName = Messages.RequestNumber)]
+        [ValidateFields(Messages.ErrorCustoms, typeof(customs), typeof(importDate))]
+        public virtual string RequestNbr { get; set; }
+
+        #endregion RequestNbr
+
+        #region LineNbr
+
+        public abstract class lineNbr : IBqlField
+        {
+        }
+
+        [PXDBInt(IsKey = true)]
+        public virtual int? LineNbr { get; set; }
+
+        #endregion LineNbr
+
+        #region ItemSold
+
+        public abstract class itemSold : PX.Data.IBqlField
+        {
+        }
+
+        [PXDBBool()]
+        [PXDefault(false)]
+        public virtual bool? ItemSold { get; set; }
+
+        #endregion ItemSold
+
+        #region BatchSold
+
+        public abstract class batchSold : PX.Data.IBqlField
+        {
+        }
+
+        [PXDBBool()]
+        [PXDefault(false)]
+        public virtual bool? BatchSold { get; set; }
+
+        #endregion BatchSold
+
+        #region NoteID
+
+        public abstract class noteID : PX.Data.IBqlField
+        {
+        }
+
+        /// <summary>
+        /// Identifier of the <see cref="PX.Data.Note">Note</see> object, associated with the document.
+        /// </summary>
+        /// <value>
+        /// Corresponds to the <see cref="PX.Data.Note.NoteID">Note.NoteID</see> field.
+        /// </value>
+        [PXNote(ShowInReferenceSelector = true)]
+        public Guid? NoteID { get; set; }
+
+        #endregion NoteID
+
+        #region tstamp
+
+        public abstract class Tstamp : PX.Data.IBqlField
+        {
+        }
+
+        protected byte[] _tstamp;
+
+        [PXDBTimestamp()]
+        public virtual byte[] tstamp
+        {
+            get
+            {
+                return this._tstamp;
+            }
+            set
+            {
+                this._tstamp = value;
+            }
+        }
+
+        #endregion tstamp
+
+        #region CreatedByID
+
+        public abstract class createdByID : PX.Data.IBqlField
+        {
+        }
+
+        protected Guid? _CreatedByID;
+
+        [PXDBCreatedByID()]
+        public virtual Guid? CreatedByID
+        {
+            get
+            {
+                return this._CreatedByID;
+            }
+            set
+            {
+                this._CreatedByID = value;
+            }
+        }
+
+        #endregion CreatedByID
+
+        #region CreatedByScreenID
+
+        public abstract class createdByScreenID : PX.Data.IBqlField
+        {
+        }
+
+        protected string _CreatedByScreenID;
+
+        [PXDBCreatedByScreenID()]
+        public virtual string CreatedByScreenID
+        {
+            get
+            {
+                return this._CreatedByScreenID;
+            }
+            set
+            {
+                this._CreatedByScreenID = value;
+            }
+        }
+
+        #endregion CreatedByScreenID
+
+        #region CreatedDateTime
+
+        public abstract class createdDateTime : PX.Data.IBqlField
+        {
+        }
+
+        protected DateTime? _CreatedDateTime;
+
+        [PXDBCreatedDateTime()]
+        public virtual DateTime? CreatedDateTime
+        {
+            get
+            {
+                return this._CreatedDateTime;
+            }
+            set
+            {
+                this._CreatedDateTime = value;
+            }
+        }
+
+        #endregion CreatedDateTime
+
+        #region LastModifiedByID
+
+        public abstract class lastModifiedByID : PX.Data.IBqlField
+        {
+        }
+
+        protected Guid? _LastModifiedByID;
+
+        [PXDBLastModifiedByID()]
+        public virtual Guid? LastModifiedByID
+        {
+            get
+            {
+                return this._LastModifiedByID;
+            }
+            set
+            {
+                this._LastModifiedByID = value;
+            }
+        }
+
+        #endregion LastModifiedByID
+
+        #region LastModifiedByScreenID
+
+        public abstract class lastModifiedByScreenID : PX.Data.IBqlField
+        {
+        }
+
+        protected string _LastModifiedByScreenID;
+
+        [PXDBLastModifiedByScreenID()]
+        public virtual string LastModifiedByScreenID
+        {
+            get
+            {
+                return this._LastModifiedByScreenID;
+            }
+            set
+            {
+                this._LastModifiedByScreenID = value;
+            }
+        }
+
+        #endregion LastModifiedByScreenID
+
+        #region LastModifiedDateTime
+
+        public abstract class lastModifiedDateTime : PX.Data.IBqlField
+        {
+        }
+
+        protected DateTime? _LastModifiedDateTime;
+
+        [PXDBLastModifiedDateTime()]
+        public virtual DateTime? LastModifiedDateTime
+        {
+            get
+            {
+                return this._LastModifiedDateTime;
+            }
+            set
+            {
+                this._LastModifiedDateTime = value;
+            }
+        }
+
+        #endregion LastModifiedDateTime
+    }
+}
---
+++ AcumaticaMX/Sql/MXINItemLotSerial.sql
@@ -0,0 +1,48 @@
+﻿If Exists (Select * From sys.objects Where object_id = OBJECT_ID(N'[MXINItemLotSerial]') And type in (N'U'))
+	Drop Table [MXINItemLotSerial]
+Go
+
+/****** Object:  Table [dbo].[MXINItemLotSerial] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MXINItemLotSerial](
+	-- Campo de soporte multiempresa
+	[CompanyID]			[int] NOT NULL DEFAULT ((0)),
+
+	-- Llaves de relacion
+	[InventoryID]		[int] NOT NULL DEFAULT ((0)),
+	[LotSerialNbr]		[nvarchar] (15) NOT NULL,
+	[RefNbr]			[nvarchar] (15) NOT NULL, 
+	-- Numero de referencia de la recepción con la cual entro por primera vez 
+	-- Datos de Informacion Aduanera
+	[Customs]			[nvarchar](25) NULL,
+	[ImportDate]		[smalldatetime] NULL,
+	[RequestNbr]		[nvarchar](50) NULL,
+	[LineNbr]			[int] NOT NULL DEFAULT ((0)),  -- Numerp de linea de la recepción (uso futura modificación)
+	[ItemSold]			[bit] NOT NULL DEFAULT ((0)),  -- Indica si el item ya ha sido vendido 
+	[BatchSold]			[bit] NOT NULL DEFAULT ((0)),  -- Indica si la información aduanera puede ser modificada
+		-- Notes support
+	[NoteID]					uniqueidentifier NULL,
+		-- handle concurrency
+    [tstamp]					timestamp NOT NULL,
+
+		-- basic audit fields
+    [CreatedByID]				uniqueidentifier NOT NULL,
+	[CreatedByScreenID]			char(8) NOT NULL,
+	[CreatedDateTime]			smalldatetime NOT NULL,
+	[LastModifiedByID]			uniqueidentifier NOT NULL,
+	[LastModifiedByScreenID]	char(8) NOT NULL,
+	[LastModifiedDateTime]      smalldatetime NOT NULL,
+
+	CONSTRAINT [MXINItemLotSerial_PK] PRIMARY KEY CLUSTERED 
+	(
+		[CompanyID]		ASC,
+		[InventoryID]   ASC,
+		[LotSerialNbr]  ASC,
+		[RefNbr]		ASC
+	)
+)
