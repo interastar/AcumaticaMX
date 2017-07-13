317f479cb0d804c4f0bdf5c0f8733c736b5c12f3
omar alvarez <omar.alvarez@interastar.com>
2017-07-13 13:22:21 -0500

    Actualización del proyecto para una nueva instancia

--- AcumaticaMX/Properties/AssemblyInfo.cs
+++ AcumaticaMX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MXObjects")]
+[assembly: AssemblyTitle("AcumaticaMX")]
 [assembly: AssemblyDescription("Paquete de Acumatica para estandarización de datos en México")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Syntegh")]
-[assembly: AssemblyProduct("MXObjects")]
+[assembly: AssemblyProduct("AcumaticaMX")]
 [assembly: AssemblyCopyright("Copyright © 2005-2016 SYNCHRONIZE TECHNOLOGY GH CONSULTING & SOLUTIONS SA DE CV")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
