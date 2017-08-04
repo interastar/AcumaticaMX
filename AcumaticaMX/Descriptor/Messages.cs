64a8e39149dc15b8e74b768da41efb2f517cb59a
omar alvarez <omar.alvarez@interastar.com>
2017-08-04 11:54:30 -0500

    Etiquetas para setup

--- AcumaticaMX/Descriptor/Messages.cs
+++ AcumaticaMX/Descriptor/Messages.cs
@@ -11,10 +11,6 @@ namespace MX.Objects
         // Textos y Etiquetas de campos
 
         #region Nombres
-
-        //public const string Etiqueta = "Valor";
-        //public const string Etiqueta = "Valor";
-        //public const string Etiqueta = "Valor";
         public const string Municipality = "Municipio / Delegación";
 
         #endregion Nombres
@@ -46,12 +42,15 @@ namespace MX.Objects
         //*** SYFEPaymentMethodExtension
         public const string PaymentMethod = "Clave Método de Pago en SAT";
 
+        #region Información Aduanera
         //*** MXPOReceipLineExt
         public const string Customs = "Aduana";
         public const string ImportDate = "Fecha de importación";
         public const string RequestNumber = "Numero de Pedimento";
         public const string ErrorCustoms = "Si ingresa Número de pedimento, debe ingresar nombre de aduana y la fecha de importación.";
         public const string ErrorImportDate = "El campo Fecha de Importacion no puede estar vacio";
+        #endregion Información Aduanera
+
         #region Catalogo de Cuentas
 
         //*** MXCESatAccountList
@@ -62,11 +61,15 @@ namespace MX.Objects
 
         #endregion Catalogo de Cuentas
 
-        #region Catalogo de Monedas
+        #region Setup
 
-        //*** MXCESatMoneyList
-        public const string MoneyCodeLabel = "Código de Monedas";
-        public const string MoneyNameLabel = "Nombre de la moneda";
-        #endregion Catalogo de Monedas
+        //*** SYFESetup
+        public const string MainBranch = "Sucursal Principal";
+        public const string Credentials = "Credenciales";
+        public const string CertificateNbr = "Número de Certificado";
+        public const string Provider = "PAC";
+        public const string ProviderUser = "Usuario del PAC";
+        public const string ProviderPassword = "Contraseña del PAC";
+        #endregion Setup
     }
 }
