a990a957b78bc403bcba8fb8b89ebcaa1903e6c3
LAPTOP-A11GDI8B\omara <omar.alvarez@interastar.com>
2017-09-10 14:22:42 -0500

    Proyecto Regimen Fiscal SQL

---
+++ AcumaticaMX/Sql/MXFESatTaxRegimList.sql
@@ -0,0 +1,34 @@
+﻿If Exists(Select* From sys.objects Where object_id = OBJECT_ID(N'[MXFESatTaxRegimList]') And type in (N'U'))
+	Drop Table [dbo].[MXFESatTaxRegimList]
+Go
+Create Table [dbo].[MXFESatTaxRegimList]
+(
+	-- multi-tenancy support
+	[CompanyID]			int NOT NULL DEFAULT ((0)),
+
+	-- surrogate/natural key
+	[TaxRegimID]	    int IDENTITY(1,1) NOT NULL,
+	[Description]		nvarchar(500) NOT NULL,
+
+	[ApplyNaturalPerson]bit null default((0)),
+	[ApplyMoralPerson]	bit null default((0)),
+	[ValidityStartDate]	smalldatetime NOT NULL,
+	[ValidityEndDate]	smalldatetime NULL,
+
+	-- handle concurrency
+	[tstamp]			timestamp NOT NULL,
+
+	-- basic audit fields
+	[CreatedByID]		uniqueidentifier NOT NULL,
+	[CreatedByScreenID]	char(8) NOT NULL,
+	[CreatedDateTime]	smalldatetime NOT NULL,
+	[LastModifiedByID]	uniqueidentifier NOT NULL,
+	[LastModifiedByScreenID]	char(8) NOT NULL,
+	[LastModifiedDateTime]		smalldatetime NOT NULL,
+
+	CONSTRAINT MXFESatTaxRegimList_PK PRIMARY KEY
+	(
+		[CompanyID] ASC,
+		[TaxRegimID] ASC
+	)
+)
