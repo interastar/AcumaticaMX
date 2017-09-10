3dbfef9d8ff4ac71e5a64658e34604ab427177ab
LAPTOP-A11GDI8B\omara <omar.alvarez@interastar.com>
2017-09-10 10:19:41 -0500

    Catálogo de numeros de pedimentos.

---
+++ AcumaticaMX/Sql/MXFESatCustomsRequestNumberList.sql
@@ -0,0 +1,35 @@
+﻿If Exists(Select* From sys.objects Where object_id = OBJECT_ID(N'[MXFESatCustomsRequestNumberList]') And type in (N'U'))
+	Drop Table [dbo].[MXFESatCustomsRequestNumberList]
+Go
+Create Table [dbo].[MXFESatCustomsRequestNumberList]
+(
+	-- multi-tenancy support
+	[CompanyID]			int NOT NULL DEFAULT ((0)),
+
+	-- surrogate/natural key
+	[Key]				uniqueidentifier NOT NULL,
+	[CustomsCD]			nvarchar(2) NOT NULL,
+
+	[Patent]			int NOT NULL,
+	[FiscalExcercise]	int NOT NULL,
+	[Qty]				nvarchar(6) NOT NULL,
+	[ValidityStartDate]	smalldatetime NOT NULL,
+	[ValidityEndDate]	smalldatetime NULL,
+
+	-- handle concurrency
+	[tstamp]			timestamp NOT NULL,
+
+	-- basic audit fields
+	[CreatedByID]		uniqueidentifier NOT NULL,
+	[CreatedByScreenID]	char(8) NOT NULL,
+	[CreatedDateTime]	smalldatetime NOT NULL,
+	[LastModifiedByID]	uniqueidentifier NOT NULL,
+	[LastModifiedByScreenID]	char(8) NOT NULL,
+	[LastModifiedDateTime]		smalldatetime NOT NULL,
+
+	CONSTRAINT MXFESatCustomsRequestNumberList_PK PRIMARY KEY
+	(
+		[CompanyID] ASC,
+		[Key] ASC
+	)
+)
