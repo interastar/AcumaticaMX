37aea0932c7e77177f4caac50c3cedac60332d71
LAPTOP-A11GDI8B\omara <omar.alvarez@interastar.com>
2017-09-20 17:54:43 -0500

    Dac que guarda el catalogo del uso del cfdi

---
+++ AcumaticaMX/Sql/MXFESatUseCFDIList.sql
@@ -0,0 +1,34 @@
+﻿If Exists(Select* From sys.objects Where object_id = OBJECT_ID(N'[MXFESatUseCFDIList]') And type in (N'U'))
+	Drop Table [dbo].[MXFESatUseCFDIList]
+Go
+Create Table [dbo].[MXFESatUseCFDIList]
+(
+	-- multi-tenancy support
+	[CompanyID]					int NOT NULL DEFAULT ((0)),
+
+	-- surrogate/natural key
+	[UseCFDI]					nvarchar(3) NOT NULL,
+	[Description]				nvarchar(500) NOT NULL,
+
+	[ApplyNaturalPerson]		bit null default((0)),
+	[ApplyMoralPerson]			bit null default((0)),
+	[ValidityStartDate]			smalldatetime NOT NULL,
+	[ValidityEndDate]			smalldatetime NULL,
+
+	-- handle concurrency
+	[tstamp]					timestamp NOT NULL,
+
+	-- basic audit fields
+	[CreatedByID]				uniqueidentifier NOT NULL,
+	[CreatedByScreenID]			char(8) NOT NULL,
+	[CreatedDateTime]			smalldatetime NOT NULL,
+	[LastModifiedByID]			uniqueidentifier NOT NULL,
+	[LastModifiedByScreenID]	char(8) NOT NULL,
+	[LastModifiedDateTime]		smalldatetime NOT NULL,
+
+	CONSTRAINT MXFESatUseCFDIList_PK PRIMARY KEY
+	(
+		[CompanyID] ASC,
+		[UseCFDI] ASC
+	)
+)
