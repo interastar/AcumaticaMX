5885881c613b0a4ba96317529f909c82ce100e23
LAPTOP-A11GDI8B\omara <omar.alvarez@interastar.com>
2017-09-25 13:36:54 -0500

    Agrega el selector de producto y sevicio a ARTran y a los productos almacenados y no almacenados.

---
+++ AcumaticaMX/DAC/MXARTranExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PX.Data;
+using PX.Objects.AR;
+namespace AcumaticaMX
+{
+    [Serializable]
+    [PXTable(typeof(ARTran.tranType),typeof(ARTran.refNbr), typeof(ARTran.lineNbr),IsOptional = true)]
+    public class MXARTranExtension : PXCacheExtension<ARTran>
+    {
+        public abstract class productServiceCD : IBqlField
+        {
+        }
+        [PXDBString]
+        [PXSelector(
+            typeof(Search<MXFESatProductServiceList.productServiceCD>),
+            typeof(MXFESatProductServiceList.description),
+            DescriptionField = typeof(MXFESatProductServiceList.description))]
+        [PXDefault]
+        [PXFormula(typeof(Selector<ARTran.inventoryID, MXINInventoryItemExtension.productServiceCD>))]
+        [PXUIField(DisplayName = Messages.ProductService)]
+        public virtual string ProductServiceCD { get; set; }
+    }
+}
---
+++ AcumaticaMX/Sql/MXARTranExtension.sql
@@ -0,0 +1,23 @@
+﻿If Exists (Select * From sys.objects Where object_id = OBJECT_ID(N'[MXARTranExtension]') And type in (N'U'))
+	Drop Table [MXARTranExtension]
+Go
+
+/****** Object:  Table [dbo].[MXARTranExtension] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MXARTranExtension](
+	-- multi-tenancy support
+	[CompanyID]				[int] NOT NULL DEFAULT ((0)),
+
+	-- surrogate/natural key
+	[TranType]				[nvarchar](3) NOT NULL,
+	[RefNbr]				[nvarchar](15) NOT NULL,
+	[LineNbr]				[int] NOT NULL DEFAULT ((0)),
+
+	-- extension fields
+	[ProductServiceCD]		[nvarchar](8) NOT NULL,
+)
