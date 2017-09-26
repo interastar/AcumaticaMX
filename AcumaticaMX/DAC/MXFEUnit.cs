ca1280a9e4c7a844706dbfc918aa386393ae6f1e
LAPTOP-A11GDI8B\omara <omar.alvarez@interastar.com>
2017-09-26 13:06:02 -0500

    Tabla para vincular las unidades con el catalogo de medidas

---
+++ AcumaticaMX/DAC/MXFEUnit.cs
@@ -0,0 +1,195 @@
+﻿using PX.Data;
+using PX.Objects.IN;
+using System;
+
+
+namespace AcumaticaMX
+{
+    public class MXFEUnit : IBqlTable
+    {
+
+        public abstract class unit : IBqlField
+        {
+        }
+        [PXDBString]
+        [PXUIField(DisplayName = Messages.Unit)]
+        public virtual string Unit { get; set; }
+
+        public abstract class measureCD : IBqlField
+        {
+        }
+        [PXSelector(
+            typeof(Search<MXFESatMeasureList.measureCD>),
+            typeof(MXFESatMeasureList.name),
+            typeof(MXFESatMeasureList.description),
+            DescriptionField = typeof(MXFESatMeasureList.name))]
+        [PXDBString]
+        [PXUIField(DisplayName = Messages.Measure)]
+        public virtual string MeasureCD { get; set; }
+
+        #region audit
+
+        #region tstamp
+
+        public abstract class Tstamp : PX.Data.IBqlField
+        {
+        }
+
+        protected byte[] _tstamp;
+
+        [PXDBTimestamp()]
+        public virtual byte[] tstamp
+        {
+            get
+            {
+                return this._tstamp;
+            }
+            set
+            {
+                this._tstamp = value;
+            }
+        }
+
+        #endregion tstamp
+
+        #region CreatedByID
+
+        public abstract class createdByID : PX.Data.IBqlField
+        {
+        }
+
+        protected Guid? _CreatedByID;
+
+        [PXDBCreatedByID()]
+        public virtual Guid? CreatedByID
+        {
+            get
+            {
+                return this._CreatedByID;
+            }
+            set
+            {
+                this._CreatedByID = value;
+            }
+        }
+
+        #endregion CreatedByID
+
+        #region CreatedByScreenID
+
+        public abstract class createdByScreenID : PX.Data.IBqlField
+        {
+        }
+
+        protected string _CreatedByScreenID;
+
+        [PXDBCreatedByScreenID()]
+        public virtual string CreatedByScreenID
+        {
+            get
+            {
+                return this._CreatedByScreenID;
+            }
+            set
+            {
+                this._CreatedByScreenID = value;
+            }
+        }
+
+        #endregion CreatedByScreenID
+
+        #region CreatedDateTime
+
+        public abstract class createdDateTime : PX.Data.IBqlField
+        {
+        }
+
+        protected DateTime? _CreatedDateTime;
+
+        [PXDBCreatedDateTime()]
+        public virtual DateTime? CreatedDateTime
+        {
+            get
+            {
+                return this._CreatedDateTime;
+            }
+            set
+            {
+                this._CreatedDateTime = value;
+            }
+        }
+
+        #endregion CreatedDateTime
+
+        #region LastModifiedByID
+
+        public abstract class lastModifiedByID : PX.Data.IBqlField
+        {
+        }
+
+        protected Guid? _LastModifiedByID;
+
+        [PXDBLastModifiedByID()]
+        public virtual Guid? LastModifiedByID
+        {
+            get
+            {
+                return this._LastModifiedByID;
+            }
+            set
+            {
+                this._LastModifiedByID = value;
+            }
+        }
+
+        #endregion LastModifiedByID
+
+        #region LastModifiedByScreenID
+
+        public abstract class lastModifiedByScreenID : PX.Data.IBqlField
+        {
+        }
+
+        protected string _LastModifiedByScreenID;
+
+        [PXDBLastModifiedByScreenID()]
+        public virtual string LastModifiedByScreenID
+        {
+            get
+            {
+                return this._LastModifiedByScreenID;
+            }
+            set
+            {
+                this._LastModifiedByScreenID = value;
+            }
+        }
+
+        #endregion LastModifiedByScreenID
+
+        #region LastModifiedDateTime
+
+        public abstract class lastModifiedDateTime : PX.Data.IBqlField
+        {
+        }
+
+        protected DateTime? _LastModifiedDateTime;
+
+        [PXDBLastModifiedDateTime()]
+        public virtual DateTime? LastModifiedDateTime
+        {
+            get
+            {
+                return this._LastModifiedDateTime;
+            }
+            set
+            {
+                this._LastModifiedDateTime = value;
+            }
+        }
+
+        #endregion LastModifiedDateTime
+
+        #endregion audit
+    }
+}
---
+++ AcumaticaMX/Sql/MXFEUnit.sql
@@ -0,0 +1,38 @@
+﻿If Exists (Select * From sys.objects Where object_id = OBJECT_ID(N'[MXFEUnit]') And type in (N'U'))
+	Drop Table [MXFEUnit]
+Go
+
+/****** Object:  Table [dbo].[MXFEUnit] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MXFEUnit](
+	-- multi-tenancy support
+	[CompanyID]					[int] NOT NULL DEFAULT ((0)),
+
+	-- surrogate/natural key
+	[Unit]						[nvarchar](6) NOT NULL,
+
+	-- extension fields
+	[MeasureCD]					[nvarchar](3) NOT NULL,
+
+	-- handle concurrency
+	[tstamp]					timestamp NOT NULL,
+
+	-- basic audit fields
+	[CreatedByID]				uniqueidentifier NOT NULL,
+	[CreatedByScreenID]			char(8) NOT NULL,
+	[CreatedDateTime]			smalldatetime NOT NULL,
+	[LastModifiedByID]			uniqueidentifier NOT NULL,
+	[LastModifiedByScreenID]	char(8) NOT NULL,
+	[LastModifiedDateTime]		smalldatetime NOT NULL,
+	
+	CONSTRAINT [MXFEUnit_PK] PRIMARY KEY CLUSTERED 
+	(
+		[CompanyID] ASC,
+		[Unit] ASC
+	)
+)
