c7cc8303ba3cb972e8892510f059b832e2ed1972
LAPTOP-A11GDI8B\omara <omar.alvarez@interastar.com>
2017-10-10 11:56:05 -0500

    Creamos una extension de impuestos para vincular el impuesto de acumatica al del sat.

---
+++ AcumaticaMX/DAC/MXTXTaxExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PX.Data;
+using PX.Objects.TX;
+namespace AcumaticaMX
+{
+    [Serializable]
+    [PXTable(typeof(Tax.taxID), IsOptional = true)]
+    public class MXTXTaxExtension : PXCacheExtension<Tax>
+    {
+        public abstract class satTax : IBqlField
+        {
+        }
+
+        [PXDBString(4, IsUnicode = true)]
+        [PXStringList(
+            new string[]
+            {
+                Common.Tax.ISR,
+                Common.Tax.IVA,
+                Common.Tax.IEPS,
+            },
+            new string[]
+            {
+                Common.Tax.ISRLabel,
+                Common.Tax.IVALabel,
+                Common.Tax.IEPSLabel,
+            }
+            )]
+        [PXUIField(DisplayName = Messages.Tax, Enabled = true)]
+        [PXDefault(Common.Tax.IVA)]
+        public virtual string SatTax { get; set; }
+    }
+}
