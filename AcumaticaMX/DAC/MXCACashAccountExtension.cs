444b5525e3918925c5ea6556279455ef92cb5d43
LAPTOP-A11GDI8B\omara <omar.alvarez@interastar.com>
2017-10-19 19:14:33 -0500

    Crea extension sobre CashAccount para asignar banco y el numero de cuenta

---
+++ AcumaticaMX/DAC/MXCACashAccountExtension.cs
@@ -0,0 +1,38 @@
+﻿using PX.Data;
+using PX.Objects.CA;
+using System;
+
+namespace AcumaticaMX
+{
+    [Serializable]
+    [PXTable(typeof(CashAccount.cashAccountID), IsOptional = true)]
+    public class MXCACashAccountExtension : PXCacheExtension<CashAccount>
+    {
+        #region Bank
+
+        public abstract class bankCD : IBqlField
+        {
+        }
+
+        [PXDBString(3, IsUnicode = true)]
+        [PXSelector(
+            typeof(Search<MXFESatBankList.banckCD>),
+            DescriptionField = typeof(MXFESatBankList.banckName))]
+        [PXUIField(DisplayName = "Banco")]
+        public virtual string BankCD { get; set; }
+
+        #endregion Bank
+
+        #region Bank Account
+
+        public abstract class bankAccount : IBqlField
+        {
+        }
+
+        [PXDBString(50, IsUnicode = true)]
+        [PXUIField(DisplayName = "Numero de cuenta bancario")]
+        public virtual string BankAccount { get; set; }
+
+        #endregion Bank Account
+    }
+}
