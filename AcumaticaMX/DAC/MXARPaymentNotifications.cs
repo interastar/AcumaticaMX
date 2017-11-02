48fbaad04e2ff6c1d9d8ed6b8fa70014adf8c153
LAPTOP-A11GDI8B\omara <omar.alvarez@interastar.com>
2017-11-02 11:36:35 -0600

    Agrega tabla para guardar la referencia de los documentos

---
+++ AcumaticaMX/DAC/MXARPaymentNotifications.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using PX.Data;
+using PX.Objects.AR;
+
+namespace AcumaticaMX
+{
+    public class MXARPaymentNotifications : IBqlTable
+    {
+        #region RefNbr
+
+        public abstract class refNbr : IBqlField
+        {
+        }
+        [PXDBString(IsKey = true)]
+        [PXParent(typeof(Select<ARPayment,
+            Where<ARPayment.refNbr,
+                Equal<Current<ARPayment.refNbr>>,
+                    And<ARPayment.docType,
+                        Equal<ARPayment.docType>>>>))]
+        [PXDBDefault(typeof(ARPayment.refNbr))]
+        public virtual string RefNbr { get; set; }
+
+        #endregion RefNbr
+
+        #region DocType
+
+        public abstract class docType : IBqlField
+        {
+        }
+        [PXDBString(IsKey = true)]
+        [PXDBDefault(typeof(ARPayment.docType))]
+        public virtual string DocType { get; set; }
+
+        #endregion DocType
+
+        #region LineNbr
+        public abstract class lineNbr : PX.Data.IBqlField
+        {
+        }
+        [PXDBIdentity(IsKey = true)]
+        public virtual int? LineNbr { get; set; }
+        #endregion LineNbr
+
+        #region Send
+
+        public abstract class send : IBqlField { }
+
+        [PXDBBool]
+        [PXDefault(true)]
+        [PXUIField(DisplayName = "Enviar por correo", Visibility = PXUIVisibility.SelectorVisible, Enabled = true)]
+        public virtual bool? Send { get; set; }
+
+        #endregion Send
+
+        #region StampedUuid
+
+        public abstract class stampedUuid : IBqlField { }
+
+        [PXDBGuid]
+        [PXUIField(DisplayName = "Uuid")]
+        public virtual Guid? StampedUuid { get; set; }
+
+        #endregion StampedUuid
+
+        #region AttachedUuid
+
+        public abstract class attachedUuid : IBqlField { }
+
+        [PXDBGuid]
+        public virtual Guid? AttachedUuid { get; set; }
+
+        #endregion AttachedUuid
+
+        #region audit
+
+        #region tstamp
+
+        public abstract class Tstamp : PX.Data.IBqlField
+        {
+        }
+
+        protected byte[] _tstamp;
+
+        [PXDBTimestamp()]
+        public virtual byte[] tstamp
+        {
+            get
+            {
+                return this._tstamp;
+            }
+            set
+            {
+                this._tstamp = value;
+            }
+        }
+
+        #endregion tstamp
+
+        #region CreatedByID
+
+        public abstract class createdByID : PX.Data.IBqlField
+        {
+        }
+
+        protected Guid? _CreatedByID;
+
+        [PXDBCreatedByID()]
+        public virtual Guid? CreatedByID
+        {
+            get
+            {
+                return this._CreatedByID;
+            }
+            set
+            {
+                this._CreatedByID = value;
+            }
+        }
+
+        #endregion CreatedByID
+
+        #region CreatedByScreenID
+
+        public abstract class createdByScreenID : PX.Data.IBqlField
+        {
+        }
+
+        protected string _CreatedByScreenID;
+
+        [PXDBCreatedByScreenID()]
+        public virtual string CreatedByScreenID
+        {
+            get
+            {
+                return this._CreatedByScreenID;
+            }
+            set
+            {
+                this._CreatedByScreenID = value;
+            }
+        }
+
+        #endregion CreatedByScreenID
+
+        #region CreatedDateTime
+
+        public abstract class createdDateTime : PX.Data.IBqlField
+        {
+        }
+
+        protected DateTime? _CreatedDateTime;
+
+        [PXDBCreatedDateTime()]
+        public virtual DateTime? CreatedDateTime
+        {
+            get
+            {
+                return this._CreatedDateTime;
+            }
+            set
+            {
+                this._CreatedDateTime = value;
+            }
+        }
+
+        #endregion CreatedDateTime
+
+        #region LastModifiedByID
+
+        public abstract class lastModifiedByID : PX.Data.IBqlField
+        {
+        }
+
+        protected Guid? _LastModifiedByID;
+
+        [PXDBLastModifiedByID()]
+        public virtual Guid? LastModifiedByID
+        {
+            get
+            {
+                return this._LastModifiedByID;
+            }
+            set
+            {
+                this._LastModifiedByID = value;
+            }
+        }
+
+        #endregion LastModifiedByID
+
+        #region LastModifiedByScreenID
+
+        public abstract class lastModifiedByScreenID : PX.Data.IBqlField
+        {
+        }
+
+        protected string _LastModifiedByScreenID;
+
+        [PXDBLastModifiedByScreenID()]
+        public virtual string LastModifiedByScreenID
+        {
+            get
+            {
+                return this._LastModifiedByScreenID;
+            }
+            set
+            {
+                this._LastModifiedByScreenID = value;
+            }
+        }
+
+        #endregion LastModifiedByScreenID
+
+        #region LastModifiedDateTime
+
+        public abstract class lastModifiedDateTime : PX.Data.IBqlField
+        {
+        }
+
+        protected DateTime? _LastModifiedDateTime;
+
+        [PXDBLastModifiedDateTime()]
+        public virtual DateTime? LastModifiedDateTime
+        {
+            get
+            {
+                return this._LastModifiedDateTime;
+            }
+            set
+            {
+                this._LastModifiedDateTime = value;
+            }
+        }
+
+        #endregion LastModifiedDateTime
+
+        #endregion audit
+    }
+}
---
+++ AcumaticaMX/Sql/MXARPaymentNotifications.sql
@@ -0,0 +1,35 @@
+﻿If Exists(Select* From sys.objects Where object_id = OBJECT_ID(N'[MXARPaymentNotifications]') And type in (N'U'))
+	Drop Table [dbo].[MXARPaymentNotifications]
+Go
+Create Table [dbo].[MXARPaymentNotifications]
+(
+	-- multi-tenancy support
+	[CompanyID]				int NOT NULL DEFAULT ((0)),
+
+	-- surrogate/natural key
+	[RefNbr]				nvarchar(15)NOT NULL,
+	[DocType]				nvarchar(3) NOT NULL,
+	[LineNbr]				int IDENTITY NOT NULL,
+
+	[StampedUuid]			uniqueidentifier NULL,
+	[AttachedUuid]			uniqueidentifier NULL,
+	[Send]				bit NULL default ((1)),
+	-- handle concurrency
+	[tstamp]				timestamp NOT NULL,
+
+	-- basic audit fields
+	[CreatedByID]			uniqueidentifier NOT NULL,
+	[CreatedByScreenID]		char(8) NOT NULL,
+	[CreatedDateTime]		smalldatetime NOT NULL,
+	[LastModifiedByID]		uniqueidentifier NOT NULL,
+	[LastModifiedByScreenID]char(8) NOT NULL,
+	[LastModifiedDateTime]	smalldatetime NOT NULL,
+
+	CONSTRAINT MXARPaymentNotifications_PK PRIMARY KEY
+	(
+		[CompanyID] ASC,
+		[RefNbr] ASC,
+		[DocType] ASC,
+		[LineNbr] ASC
+	)
+)
