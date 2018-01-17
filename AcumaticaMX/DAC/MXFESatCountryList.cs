91793b8b06117c7508e3cd5a3564fa95c602daa7
omar alvarez <omar.alvarez@interastar.com>
2018-01-16 19:06:17 -0600

    Catalogo de paises

---
+++ AcumaticaMX/DAC/MXFESatCountryList.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using PX.Data;
+
+namespace AcumaticaMX
+{
+    public class MXFESatCountryList : IBqlTable
+    {
+        #region Country
+        public abstract class countryCD : IBqlField
+        {
+        }
+        [PXDBString(3, IsKey = true, IsUnicode = true, InputMask = ">CCC")]
+        [PXUIField(DisplayName = Messages.Country)]
+        public virtual string CountryCD { get; set; }
+        #endregion Country
+
+        #region Name
+        public abstract class name : IBqlField
+        {
+        }
+        [PXDBString(250, IsUnicode = true)]
+        [PXUIField(DisplayName = Messages.Name)]
+        public virtual string Name { get; set; }
+        #endregion Name
+
+        #region audit
+
+        #region tstamp
+
+        public abstract class Tstamp : PX.Data.IBqlField
+        {
+        }
+
+        protected byte[] _tstamp;
+
+        [PXDBTimestamp()]
+        public virtual byte[] tstamp
+        {
+            get
+            {
+                return this._tstamp;
+            }
+            set
+            {
+                this._tstamp = value;
+            }
+        }
+
+        #endregion tstamp
+
+        #region CreatedByID
+
+        public abstract class createdByID : PX.Data.IBqlField
+        {
+        }
+
+        protected Guid? _CreatedByID;
+
+        [PXDBCreatedByID()]
+        public virtual Guid? CreatedByID
+        {
+            get
+            {
+                return this._CreatedByID;
+            }
+            set
+            {
+                this._CreatedByID = value;
+            }
+        }
+
+        #endregion CreatedByID
+
+        #region CreatedByScreenID
+
+        public abstract class createdByScreenID : PX.Data.IBqlField
+        {
+        }
+
+        protected string _CreatedByScreenID;
+
+        [PXDBCreatedByScreenID()]
+        public virtual string CreatedByScreenID
+        {
+            get
+            {
+                return this._CreatedByScreenID;
+            }
+            set
+            {
+                this._CreatedByScreenID = value;
+            }
+        }
+
+        #endregion CreatedByScreenID
+
+        #region CreatedDateTime
+
+        public abstract class createdDateTime : PX.Data.IBqlField
+        {
+        }
+
+        protected DateTime? _CreatedDateTime;
+
+        [PXDBCreatedDateTime()]
+        public virtual DateTime? CreatedDateTime
+        {
+            get
+            {
+                return this._CreatedDateTime;
+            }
+            set
+            {
+                this._CreatedDateTime = value;
+            }
+        }
+
+        #endregion CreatedDateTime
+
+        #region LastModifiedByID
+
+        public abstract class lastModifiedByID : PX.Data.IBqlField
+        {
+        }
+
+        protected Guid? _LastModifiedByID;
+
+        [PXDBLastModifiedByID()]
+        public virtual Guid? LastModifiedByID
+        {
+            get
+            {
+                return this._LastModifiedByID;
+            }
+            set
+            {
+                this._LastModifiedByID = value;
+            }
+        }
+
+        #endregion LastModifiedByID
+
+        #region LastModifiedByScreenID
+
+        public abstract class lastModifiedByScreenID : PX.Data.IBqlField
+        {
+        }
+
+        protected string _LastModifiedByScreenID;
+
+        [PXDBLastModifiedByScreenID()]
+        public virtual string LastModifiedByScreenID
+        {
+            get
+            {
+                return this._LastModifiedByScreenID;
+            }
+            set
+            {
+                this._LastModifiedByScreenID = value;
+            }
+        }
+
+        #endregion LastModifiedByScreenID
+
+        #region LastModifiedDateTime
+
+        public abstract class lastModifiedDateTime : PX.Data.IBqlField
+        {
+        }
+
+        protected DateTime? _LastModifiedDateTime;
+
+        [PXDBLastModifiedDateTime()]
+        public virtual DateTime? LastModifiedDateTime
+        {
+            get
+            {
+                return this._LastModifiedDateTime;
+            }
+            set
+            {
+                this._LastModifiedDateTime = value;
+            }
+        }
+
+        #endregion LastModifiedDateTime
+
+        #endregion audit
+    }
+}
