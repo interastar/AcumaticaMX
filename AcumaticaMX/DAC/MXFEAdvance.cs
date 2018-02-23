12475d4482a69c8b3802f56d6d373144f02dfc11
omar alvarez <omar.alvarez@interastar.com>
2018-02-23 16:00:37 -0600

    Guarda la relacion entre anticipos y facturas de distribucion

---
+++ AcumaticaMX/DAC/MXFEAdvance.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using PX.Data;
+
+namespace AcumaticaMX
+{
+    public class MXFEAdvance : IBqlTable
+    {
+        #region DocType
+
+        public abstract class docType : IBqlField
+        {
+        }
+        [PXDBString(3, IsKey = true)]
+        public virtual string DocType { get; set; }
+
+        #endregion DocType
+
+        #region RefNbr
+
+        public abstract class refNbr : IBqlField
+        {
+        }
+        [PXDBString(15, IsKey = true)]
+        public virtual string RefNbr { get; set; }
+
+        #endregion RefNbr
+
+        #region AdvanceDocType
+
+        public abstract class advanceDocType : IBqlField
+        {
+        }
+        [PXDBString(3, IsKey = true)]
+        public virtual string AdvanceDocType { get; set; }
+
+        #endregion AdvanceDocType
+
+        #region AdvanceRefNbr
+
+        public abstract class advanceRefNbr : IBqlField
+        {
+        }
+        [PXDBString(15, IsKey = true)]
+        public virtual string AdvanceRefNbr { get; set; }
+
+        #endregion AdvanceRefNbr
+
+        #region audit
+
+        #region tstamp
+
+        public abstract class Tstamp : PX.Data.IBqlField
+        {
+        }
+
+        protected byte[] _tstamp;
+
+        [PXDBTimestamp()]
+        public virtual byte[] tstamp
+        {
+            get
+            {
+                return this._tstamp;
+            }
+            set
+            {
+                this._tstamp = value;
+            }
+        }
+
+        #endregion tstamp
+
+        #region CreatedByID
+
+        public abstract class createdByID : PX.Data.IBqlField
+        {
+        }
+
+        protected Guid? _CreatedByID;
+
+        [PXDBCreatedByID()]
+        public virtual Guid? CreatedByID
+        {
+            get
+            {
+                return this._CreatedByID;
+            }
+            set
+            {
+                this._CreatedByID = value;
+            }
+        }
+
+        #endregion CreatedByID
+
+        #region CreatedByScreenID
+
+        public abstract class createdByScreenID : PX.Data.IBqlField
+        {
+        }
+
+        protected string _CreatedByScreenID;
+
+        [PXDBCreatedByScreenID()]
+        public virtual string CreatedByScreenID
+        {
+            get
+            {
+                return this._CreatedByScreenID;
+            }
+            set
+            {
+                this._CreatedByScreenID = value;
+            }
+        }
+
+        #endregion CreatedByScreenID
+
+        #region CreatedDateTime
+
+        public abstract class createdDateTime : PX.Data.IBqlField
+        {
+        }
+
+        protected DateTime? _CreatedDateTime;
+
+        [PXDBCreatedDateTime()]
+        public virtual DateTime? CreatedDateTime
+        {
+            get
+            {
+                return this._CreatedDateTime;
+            }
+            set
+            {
+                this._CreatedDateTime = value;
+            }
+        }
+
+        #endregion CreatedDateTime
+
+        #region LastModifiedByID
+
+        public abstract class lastModifiedByID : PX.Data.IBqlField
+        {
+        }
+
+        protected Guid? _LastModifiedByID;
+
+        [PXDBLastModifiedByID()]
+        public virtual Guid? LastModifiedByID
+        {
+            get
+            {
+                return this._LastModifiedByID;
+            }
+            set
+            {
+                this._LastModifiedByID = value;
+            }
+        }
+
+        #endregion LastModifiedByID
+
+        #region LastModifiedByScreenID
+
+        public abstract class lastModifiedByScreenID : PX.Data.IBqlField
+        {
+        }
+
+        protected string _LastModifiedByScreenID;
+
+        [PXDBLastModifiedByScreenID()]
+        public virtual string LastModifiedByScreenID
+        {
+            get
+            {
+                return this._LastModifiedByScreenID;
+            }
+            set
+            {
+                this._LastModifiedByScreenID = value;
+            }
+        }
+
+        #endregion LastModifiedByScreenID
+
+        #region LastModifiedDateTime
+
+        public abstract class lastModifiedDateTime : PX.Data.IBqlField
+        {
+        }
+
+        protected DateTime? _LastModifiedDateTime;
+
+        [PXDBLastModifiedDateTime()]
+        public virtual DateTime? LastModifiedDateTime
+        {
+            get
+            {
+                return this._LastModifiedDateTime;
+            }
+            set
+            {
+                this._LastModifiedDateTime = value;
+            }
+        }
+
+        #endregion LastModifiedDateTime
+
+        #endregion audit
+    }
+}
---
+++ AcumaticaMX/Sql/MXFEAdvance.sql
@@ -0,0 +1,42 @@
+﻿If Exists(Select* From sys.objects Where object_id = OBJECT_ID(N'[MXFEAdvance]') And type in (N'U'))
+	Drop Table [dbo].[MXFEAdvance]
+Go
+/****** Object:  Table [dbo].[MXFEAdvance] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+Create Table [dbo].[MXFEAdvance]
+(
+	-- multi-tenancy support
+	[CompanyID]				int NOT NULL DEFAULT ((0)),
+	
+	
+	[RefNbr]				nvarchar(15) NOT NULL,
+	[DocType]				nvarchar(3) NOT NULL,
+	[AdvanceRefNbr]			nvarchar(15) NOT NULL,
+	[AdvanceDocType]		nvarchar(3) NOT NULL,
+
+	-- Notes support
+	[NoteID]				uniqueidentifier NULL,
+	-- handle concurrency
+	[tstamp]				timestamp NOT NULL,
+
+	-- basic audit fields
+	[CreatedByID]			uniqueidentifier NOT NULL,
+	[CreatedByScreenID]		char(8) NOT NULL,
+	[CreatedDateTime]		smalldatetime NOT NULL,
+	[LastModifiedByID]		uniqueidentifier NOT NULL,
+	[LastModifiedByScreenID]char(8) NOT NULL,
+	[LastModifiedDateTime]	smalldatetime NOT NULL,
+
+	CONSTRAINT [MXFEAdvance_PK] PRIMARY KEY CLUSTERED 
+	(
+		[CompanyID] ASC,
+		[RefNbr] ASC,
+		[DocType] ASC,
+		[AdvanceRefNbr] ASC,
+		[AdvanceDocType] ASC
+	)
+)
