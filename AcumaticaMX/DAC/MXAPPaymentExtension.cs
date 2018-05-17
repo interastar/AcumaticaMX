0a0085c3ebb7f78fe53aaccd1646b809ea2572c9
omar alvarez <omar.alvarez@interastar.com>
2018-05-17 18:27:43 -0500

    Sobreescribe la propiedad  AmountToWords

---
+++ AcumaticaMX/DAC/MXAPPaymentExtension.cs
@@ -0,0 +1,24 @@
+﻿using PX.Data;
+using PX.Objects.AP;
+using System;
+
+namespace AcumaticaMX
+{
+    [Serializable]
+    [PXTable(typeof(APPayment.docType), typeof(APPayment.refNbr), IsOptional = true)]
+    public class MXAPPaymentExtension : PXCacheExtension<APPayment>
+    {
+        [PXMergeAttributes(Method = MergeMethod.Replace)]
+
+        public string AmountToWords {
+            [PXDependsOnFields(typeof(APPayment.curyOrigDocAmt), typeof(APPayment.curyID))]
+            get
+            {
+                return Convert.ToWords(this.Base.CuryOrigDocAmt, this.Base.CuryID);
+            }
+            set
+            {
+            }
+        }
+    }
+}
