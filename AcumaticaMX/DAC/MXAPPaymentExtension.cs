469efe35cf8565553a493e2a5f229ebb0eb11c41
omar alvarez <omar.alvarez@interastar.com>
2018-05-29 13:23:20 -0500

    atributo

--- AcumaticaMX/DAC/MXAPPaymentExtension.cs
+++ AcumaticaMX/DAC/MXAPPaymentExtension.cs
@@ -8,7 +8,7 @@ namespace AcumaticaMX
     [PXTable(typeof(APPayment.docType), typeof(APPayment.refNbr), IsOptional = true)]
     public class MXAPPaymentExtension : PXCacheExtension<APPayment>
     {
-        [PXMergeAttributes(Method = MergeMethod.Replace)]
+        //[PXMergeAttributes(Method = MergeMethod.Replace)]
         [ToWordsES(typeof(APPayment.curyOrigDocAmt))]
         public virtual string AmountToWords { get; set; }
     }
