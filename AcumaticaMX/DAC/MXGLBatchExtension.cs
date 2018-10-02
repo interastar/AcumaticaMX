d8f213684a07d36910e2bf3a3f7a3c47a4e7e91c
Alejandro Zaragoza <alejandro.zaragoza@interastar.com>
2018-10-02 17:55:29 -0500

    Generación de campos para la importación de XML en pólizas

---
+++ AcumaticaMX/DAC/MXGLBatchExtension.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using PX.Data;
+using PX.Objects.AR;
+using PX.Objects.GL;
+
+namespace AcumaticaMX
+{
+    [Serializable]
+    [PXTable(typeof(Batch.module),typeof(Batch.batchNbr), IsOptional = true)]
+    public class MXGLBatchExtension : PXCacheExtension<Batch>
+    {
+        #region Cfdi Relacionado
+
+        public abstract class relCfdi : IBqlField { }
+
+        [PXDBBool]
+        [PXUIField(DisplayName = "Rel. c/CFDI")]
+        public virtual bool? RelCfdi { get; set; }
+
+        #endregion Cfdi Relacionado
+
+        #region RFC
+
+        public abstract class taxRegistrationID : PX.Data.IBqlField { }
+
+        [PXDBString(50, IsUnicode = true)]
+        [PXUIField(DisplayName = "RFC", Visibility = PXUIVisibility.SelectorVisible)]
+        public string TaxRegistrationID { get; set; }
+
+        #endregion RFC
+
+        #region Nombre
+
+        public abstract class name : PX.Data.IBqlField { }
+
+        [PXDBString(100, IsUnicode = true)]
+        [PXUIField(DisplayName = "Nombre", Visibility = PXUIVisibility.SelectorVisible)]
+        public virtual string Name { get; set; }
+
+        #endregion
+
+        #region Folio
+
+        public abstract class folio : IBqlField { }
+
+        [PXDBString(25, IsFixed = false, IsUnicode = true)]
+        [PXUIField(DisplayName = "Folio", Visibility = PXUIVisibility.SelectorVisible)]
+        public virtual string Folio { get; set; }
+        #endregion Folio
+
+        #region Uuid
+
+        public abstract class uuid : IBqlField { }
+
+        [PXDBGuid()]
+        [PXUIField(DisplayName = "UUID", Visibility = PXUIVisibility.SelectorVisible)]
+        public virtual Guid? Uuid { get; set; }
+
+        #endregion Uuid
+
+        #region Fecha Documento
+
+        public abstract class docDate : IBqlField { }
+
+        [PXDBDate()]
+        [PXUIField(DisplayName = "Fecha Doc.")]
+        public virtual DateTime? DocDate { get; set; }
+
+        #endregion Fecha Documento
+
+        #region Total Impuesto
+
+        public abstract class totalTaxes : IBqlField { }
+
+        [PXDBDecimal]
+        [PXUIField(DisplayName = "Total de Impuestos")]
+        public virtual decimal? TotalTaxes { get; set; }
+
+        #endregion Total Impuesto
+
+        #region Total Monto
+
+        public abstract class totalAmount : IBqlField { }
+
+        [PXDBDecimal]
+        [PXUIField(DisplayName = "Monto total")]
+        public virtual decimal? TotalAmount { get; set; }
+
+        #endregion Total Monto
+
+        #region Tipo Documento
+
+        public abstract class documentType : IBqlField { }
+
+        [PXDBString(1, IsFixed = false, IsUnicode = true)]
+        //Desconozco los tipos de documento
+        [PXStringList(
+            new string[]
+            {
+                Common.DocumentType.Cfdi,
+                Common.DocumentType.CfdCbb,
+                Common.DocumentType.Foreign,
+            },
+            new string[]
+            {
+                Common.DocumentType.CfdiLabel,
+                Common.DocumentType.CfdCbbLabel,
+                Common.DocumentType.ForeignLabel,
+            }
+            )]
+        [PXUIField(DisplayName = "Tipo de documento")]
+        public virtual string DocumentType { get; set; }
+
+        #endregion Tipo Documento
+    }
+}
---
+++ AcumaticaMX/Sql/MXGLBatchExtension.sql
@@ -0,0 +1,41 @@
+﻿If Exists (Select * From sys.objects Where object_id = OBJECT_ID(N'[MXGLBatchExtension]') And type in (N'U'))
+	Drop Table [MXGLBatchExtension]
+Go
+
+/****** Object:  Table [dbo].[MXGLBatchExtension] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MXGLBatchExtension](
+	-- Campo de soporte multiempresa
+	[CompanyID]			[int] NOT NULL DEFAULT ((0)),
+
+	-- Llaves de relación con documento contable (APRegister en Acumatica)
+	[Module]			char(2) NOT NULL,
+	[BatchNbr]			nvarchar(15) NOT NULL,
+
+	--CFDI Relacionado
+	[RelCfdi]			BIT NULL DEFAULT ((0)),
+
+	-- Datos del comprobante fiscal
+	[TaxRegistrationID] [nvarchar](50) NULL,
+	[Name]				[nvarchar](100) NULL,
+	[Folio]				[nvarchar](25) NULL,
+	[Uuid]				[uniqueidentifier] NULL,
+	[DocDate]			[smalldatetime] NULL,
+	[TotalTaxes]		[decimal](19, 4) NULL,
+	[TotalAmount]		[decimal](19, 4) NULL,
+	[DocumentType]		[nvarchar](1) NULL,
+
+	CONSTRAINT [MXGLBatchExtension_PK] PRIMARY KEY CLUSTERED 
+	(
+		[CompanyID] ASC,
+		[Module] ASC,
+		[BatchNbr] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
