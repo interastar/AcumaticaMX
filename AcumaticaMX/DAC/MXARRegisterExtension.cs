8e743166d480914a40d5b0504d77647c56cb6f6e
omar alvarez <omar.alvarez@interastar.com>
2018-10-03 18:07:18 -0500

    Elimina el campo payment expiration date

--- AcumaticaMX/DAC/MXARRegisterExtension.cs
+++ AcumaticaMX/DAC/MXARRegisterExtension.cs
@@ -376,15 +376,7 @@ namespace AcumaticaMX
         [PXUIField(DisplayName = Messages.OperationNbr)]
         public virtual string OperationNbr { get; set; }
         #endregion Numero de Operacion
-
-        #region PaymentExpirationDate
-
-        public abstract class paymentExpirationDate : IBqlField { }
-        [PXDBDateAndTime(PreserveTime = true)]
-        public virtual DateTime? PaymentExpirationDate { get; set; }
-
-        #endregion PaymentExpirationDate
-
+        
         // Comercio exterior
 
         #region External Trade
