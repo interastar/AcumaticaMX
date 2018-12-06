8c6ff53f2b98aa335cb54de7b4ff3ccf9baa1012
omar alvarez <omar.alvarez@interastar.com>
2018-12-06 16:41:50 -0600

    Agrega extension para SOShipment

---
+++ AcumaticaMX/DAC/MXSOShipmentADExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using PX.Data;
+using PX.Objects.SO;
+namespace AcumaticaMX
+{
+    [Serializable]
+    [PXTable(typeof(SOShipment.shipmentNbr), IsOptional = true)]
+    public class MXSOShipmentADExtension : PXCacheExtension<SOShipment>
+    {
+
+        #region EarlyInvoice
+
+        [PXDBBool]
+        [PXDefault(false, PersistingCheck = PXPersistingCheck.Nothing)]
+        [PXUIField(DisplayName = "Factura anticipada", IsReadOnly = true)]
+        public bool? EarlyInvoice { get; set; }
+
+        public class earlyInvoice : IBqlField { }
+
+        #endregion EarlyInvoice
+    }
+}
---
+++ AcumaticaMX/Sql/MXSOShipmentADExtension.sql
@@ -0,0 +1,28 @@
+﻿If Exists (Select * From sys.objects Where object_id = OBJECT_ID(N'[MXSOShipmentADExtension]') And type in (N'U'))
+	Drop Table [MXSOShipmentADExtension]
+Go
+
+/****** Object:  Table [dbo].[MXSOShipmentADExtension] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MXSOShipmentADExtension](
+	-- Campo de soporte multiempresa
+	[CompanyID]			[int] NOT NULL DEFAULT ((0)),
+
+	[ShipmentNbr]			char(15) NOT NULL,
+
+	-- Nuevos campos extendidos
+	[EarlyInvoice]			bit NULL DEFAULT ((0)),
+
+	CONSTRAINT [MXSOShipmentADExtension_PK] PRIMARY KEY CLUSTERED 
+	(
+		[CompanyID] ASC,
+		[ShipmentNbr] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
